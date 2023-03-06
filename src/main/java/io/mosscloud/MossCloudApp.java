34c195670c5d479fbcae75f0ff63945615cab3a5
Bartłomiej Juda <bartekjkk@gmail.com>
2023-03-06 14:50:10 +0100

    after wrong input you can try again

--- src/main/java/io/mosscloud/MossCloudApp.java
+++ src/main/java/io/mosscloud/MossCloudApp.java
@@ -13,23 +13,30 @@ import java.util.Scanner;
 public class MossCloudApp {
     public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Type 'prime' to count prime numbers or 'fibonacci' to count fibonacci numbers ");
-        String option = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
-        if (Objects.equals(option, "prime")) {
-            GetRangeOfPrimeNumbers getRangeOfPrimeNumbers = new GetRangeOfPrimeNumbers();
-            int minNumberOfRange = getRangeOfPrimeNumbers.getMinNumberOfRange();
-            int maxNumberOfRange = getRangeOfPrimeNumbers.getMaxNumberOfRange();
-            CountPrimeNumbers countPrimeNumbers = new CountPrimeNumbers();
-            countPrimeNumbers.printPrimeNumbers(minNumberOfRange, maxNumberOfRange);
-        } else if (Objects.equals(option, "fibonacci")){
-            System.out.println(" input 1 to save numbers to file");
-            System.out.println(" input 2 to print numbers");
-            int consumerStrategyNumber= scanner.nextInt();
-            ConsumerStrategyType consumerStrategyType = ConsumerStrategyType.values()[consumerStrategyNumber-1];
-            FibonacciScanner fibonaccscanner = new FibonacciScanner();
-            int howManyNumbers = fibonaccscanner.getHowManyNumbers();
-            CountFibonacci count = new CountFibonacci(consumerStrategyType);
-            count.countFibonacci(howManyNumbers);
+        while(true){
+            System.out.println("Type 'prime' to count prime numbers or 'fibonacci' to count fibonacci numbers ");
+            String option = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+            if (Objects.equals(option, "prime")) {
+                GetRangeOfPrimeNumbers getRangeOfPrimeNumbers = new GetRangeOfPrimeNumbers();
+                int minNumberOfRange = getRangeOfPrimeNumbers.getMinNumberOfRange();
+                int maxNumberOfRange = getRangeOfPrimeNumbers.getMaxNumberOfRange();
+                CountPrimeNumbers countPrimeNumbers = new CountPrimeNumbers();
+                countPrimeNumbers.printPrimeNumbers(minNumberOfRange, maxNumberOfRange);
+                break;
+            } else if (Objects.equals(option, "fibonacci")) {
+                System.out.println(" input 1 to save numbers to file");
+                System.out.println(" input 2 to print numbers");
+                int consumerStrategyNumber = scanner.nextInt();
+                ConsumerStrategyType consumerStrategyType = ConsumerStrategyType.values()[consumerStrategyNumber - 1];
+                FibonacciScanner fibonaccscanner = new FibonacciScanner();
+                int howManyNumbers = fibonaccscanner.getHowManyNumbers();
+                CountFibonacci count = new CountFibonacci(consumerStrategyType);
+                count.countFibonacci(howManyNumbers);
+                break;
+            }else {
+                System.out.println("You typed " + option + " instead of 'prime' or 'fibonacci'");
+                System.out.println("Please try again");
+            }
         }
     }
 }
