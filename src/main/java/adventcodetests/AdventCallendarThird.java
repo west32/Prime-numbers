2587df7f9751714352d3836b952ad1212bcac1b6
Bartłomiej Juda <bartekjkk@gmail.com>
2023-04-20 15:11:17 +0200

    adventCallendar third

--- src/main/java/adventcodetests/AdventCallendarThird.java
+++ src/main/java/adventcodetests/AdventCallendarThird.java
@@ -1,5 +1,20 @@
 package adventcodetests;
 
 public class AdventCallendarThird {
+    public static void main(String[] args) {
+        int horizontal = 0;
+        int depth = 0;
+        String direction = "";
+        int durationInMeters = 0;
+
+        switch (direction) {
+            case "forward" -> horizontal += durationInMeters;
+            case "up" -> depth -= durationInMeters;
+            case "down" -> depth += durationInMeters;
+        }
+
+
+
+    }
 
 }
